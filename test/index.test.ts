960ab41afb15efbe9f8fbf36020afd8462b97607
Tom Tang <me@qwerzl.me>
2024-11-18 20:02:50 +0800

    feat: retry fetching font metadata on network error (#41)

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from 'vitest'
-import { createUnifont, defineFontProvider } from '../src'
+import { createUnifont, defineFontProvider, providers } from '../src'
 
 describe('unifont', () => {
   it('works with no providers', async () => {
@@ -47,5 +47,30 @@ describe('unifont', () => {
       expect.objectContaining({}),
     )
     error.mockRestore()
+  })
+
+  it('retries on network errors', async () => {
+    const warn = vi.spyOn(console, 'warn').mockImplementation(() => {})
+    const error = vi.spyOn(console, 'error').mockImplementation(() => {})
+    globalThis.fetch = vi.fn(() =>
+      Promise.reject(new Error('Network Error')),
+    )
+    const unifont = await createUnifont([
+      providers.google(),
+    ])
+    const { fonts } = await unifont.resolveFont('Poppins')
+    expect(fonts).toMatchInlineSnapshot(`[]`)
+    expect(console.warn).toHaveBeenCalledTimes(3)
+    expect(console.warn).toHaveBeenNthCalledWith(1, 'Could not fetch from `https://fonts.google.com/metadata/fonts`. Will retry in `1000ms`. `3` retries left.')
+    expect(console.warn).toHaveBeenNthCalledWith(2, 'Could not fetch from `https://fonts.google.com/metadata/fonts`. Will retry in `1000ms`. `2` retries left.')
+    expect(console.warn).toHaveBeenNthCalledWith(3, 'Could not fetch from `https://fonts.google.com/metadata/fonts`. Will retry in `1000ms`. `1` retries left.')
+    expect(console.error).toHaveBeenCalledWith(
+      'Could not initialize provider `google`. `unifont` will not be able to process fonts provided by this provider.',
+      expect.objectContaining({}),
+    )
+    warn.mockRestore()
+    error.mockRestore()
+    // @ts-expect-error globalThis.fetch is altered
+    globalThis.fetch.mockRestore()
   })
 })
