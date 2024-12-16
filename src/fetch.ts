09c524eda088f2d2a0155f912f50e7adad0fde65
Daniel Roe <daniel@roe.dev>
2024-12-16 09:46:22 +0000

    refactor: slight rename

--- src/fetch.ts
+++ src/fetch.ts
@@ -6,26 +6,26 @@ interface Mini$FetchOptions extends RequestInit {
   retryDelay?: number
 }
 
-function mini$fetch<T = unknown>(url: string, options?: Mini$FetchOptions): Promise<T> {
-  let finalURL = url
+function mini$fetch<T = unknown>(_url: string, options?: Mini$FetchOptions): Promise<T> {
+  let url = _url
   if (options?.baseURL) {
-    finalURL = options.baseURL + finalURL
+    url = options.baseURL + url
   }
   if (options?.query) {
     const params = new URLSearchParams(options.query)
-    finalURL = `${finalURL}?${params.toString()}`
+    url = `${url}?${params.toString()}`
   }
 
   const retries = options?.retries ?? 3
   const retryDelay = options?.retryDelay ?? 1000
 
-  return fetch(finalURL, options)
+  return fetch(url, options)
     .then(r => options?.responseType === 'json' ? r.json() : r.text())
     .catch((err) => {
       if (retries <= 0) {
         throw err
       }
-      console.warn(`Could not fetch from \`${finalURL}\`. Will retry in \`${retryDelay}ms\`. \`${retries}\` retries left.`)
+      console.warn(`Could not fetch from \`${url}\`. Will retry in \`${retryDelay}ms\`. \`${retries}\` retries left.`)
       return new Promise(resolve => setTimeout(resolve, retryDelay))
         .then(() => mini$fetch(url, { ...options, retries: retries - 1 }))
     }) as Promise<T>
