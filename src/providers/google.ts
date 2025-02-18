dd9243e0960343ee9f3da6bb553cee99e35b056b
qwerzl <me@qwerzl.me>
2025-02-18 15:54:41 +0800

    feat: return font file priority

--- src/providers/google.ts
+++ src/providers/google.ts
@@ -1,4 +1,4 @@
-import type { ResolveFontOptions } from '../types'
+import type { FontFaceData, ResolveFontOptions } from '../types'
 
 import { hash } from 'ohash'
 import { extractFontFaceData } from '../css/parse'
@@ -29,7 +29,7 @@ export default defineFontProvider<ProviderOption>('google', async (_options = {}
 
   const userAgents = {
     woff2: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-    ttf: 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.54.16 (KHTML, like Gecko) Version/5.1.4 Safari/534.54.16',
+    woff: 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.54.16 (KHTML, like Gecko) Version/5.1.4 Safari/534.54.16',
   // eot: 'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)',
   // woff: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0',
   // svg: 'Mozilla/4.0 (iPad; CPU OS 4_0_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/4.1 Mobile/9A405 Safari/7534.48.3',
@@ -65,20 +65,28 @@ export default defineFontProvider<ProviderOption>('google', async (_options = {}
       resolvedAxes.push(axis)
     }
 
-    let css = ''
+    let priority = 0
+    const resolvedFontFaceData: FontFaceData[] = []
 
     for (const extension in userAgents) {
-      css += await $fetch<string>('/css2', {
+      const data = extractFontFaceData(await $fetch<string>('/css2', {
         baseURL: 'https://fonts.googleapis.com',
         headers: { 'user-agent': userAgents[extension as keyof typeof userAgents] },
         query: {
           family: `${family}:${resolvedAxes.join(',')}@${resolvedVariants.join(';')}`,
         },
+      }))
+      data.map((f) => {
+        f.meta ??= {}
+        f.meta.priority = priority
+        return f
       })
+      resolvedFontFaceData.push(...data)
+      priority++
     }
 
     // TODO: support subsets
-    return extractFontFaceData(css)
+    return resolvedFontFaceData
   }
 
   return {
