94c48aa2dc413da2e8c321c2d5b3d4faff2e1d2e
qwerzl <me@qwerzl.me>
2025-06-11 15:51:48 +0800

    refactor: use ofetch

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,3 +1,6 @@
+import { ofetch } from 'ofetch'
+import * as fetchLib from './fetch'
+
 interface Mini$FetchOptions extends RequestInit {
   baseURL?: string
   responseType?: 'json' | 'text'
@@ -6,29 +9,45 @@ interface Mini$FetchOptions extends RequestInit {
   retryDelay?: number
 }
 
-function mini$fetch<T = unknown>(_url: string, options?: Mini$FetchOptions): Promise<T> {
-  let url = _url
-  if (options?.baseURL) {
-    url = options.baseURL + url
-  }
-  if (options?.query) {
-    const params = new URLSearchParams(options.query)
-    url = `${url}?${params.toString()}`
-  }
+export function mini$fetch<T = unknown>(url: string, options?: Mini$FetchOptions): Promise<T> {
+  // let url = _url
+  // if (options?.baseURL) {
+  //   url = options.baseURL + url
+  // }
+  // if (options?.query) {
+  //   const params = new URLSearchParams(options.query)
+  //   url = `${url}?${params.toString()}`
+  // }
 
   const retries = options?.retries ?? 3
   const retryDelay = options?.retryDelay ?? 1000
 
-  return fetch(url, options)
-    .then(r => options?.responseType === 'json' ? r.json() : r.text())
-    .catch((err) => {
-      if (retries <= 0) {
-        throw err
-      }
-      console.warn(`Could not fetch from \`${url}\`. Will retry in \`${retryDelay}ms\`. \`${retries}\` retries left.`)
-      return new Promise(resolve => setTimeout(resolve, retryDelay))
-        .then(() => mini$fetch(_url, { ...options, retries: retries - 1 }))
-    }) as Promise<T>
+  // console.error(url)
+
+  // return fetch(url, options)
+  //   .then(r => options?.responseType === 'json' ? r.json() : r.text())
+  //   .catch((err) => {
+  //     if (retries <= 0) {
+  //       throw err
+  //     }
+  //     console.warn(`Could not fetch from \`${url}\`. Will retry in \`${retryDelay}ms\`. \`${retries}\` retries left.`)
+  //     return new Promise(resolve => setTimeout(resolve, retryDelay))
+  //       .then(() => mini$fetch(_url, { ...options, retries: retries - 1 }))
+  //   }) as Promise<T>
+
+  return ofetch(url, {
+    baseURL: options?.baseURL,
+    query: options?.query,
+    responseType: options?.responseType ?? 'text',
+    headers: options?.headers,
+  }).catch((err) => {
+    if (retries <= 0) {
+      throw err
+    }
+    console.warn(`Could not fetch from \`${(options?.baseURL ?? '') + url}\`. Will retry in \`${retryDelay}ms\`. \`${retries}\` retries left.`)
+    return new Promise(resolve => setTimeout(resolve, retryDelay))
+      .then(() => fetchLib.mini$fetch(url, { ...options, retries: retries - 1 }))
+  }) as Promise<T>
 }
 
 export const $fetch = Object.assign(mini$fetch, {
