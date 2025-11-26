4c78160932ac05a274917eddaacc1205fb99aa44
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-11-26 14:30:23 +0100

    feat(bunny): filter subsets (#298)

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import type { ProviderDefinition, ProviderFactory } from './types'
+import { findAll, generate, parse } from 'css-tree'
 
 export function defineFontProvider<TName extends string, TOptions extends Record<string, any> = never>(name: TName, provider: ProviderDefinition<TOptions>): ProviderFactory<TName, TOptions> {
   return ((options: TOptions) => Object.assign(provider.bind(null, options || {} as TOptions), { _name: name, _options: options })) as ProviderFactory<TName, TOptions>
@@ -44,4 +45,33 @@ export function prepareWeights({
     weight,
     variable: weight.includes(' '),
   }))
+}
+
+export function splitCssIntoSubsets(input: string): { subset: string | null, css: string }[] {
+  const data: { subset: string | null, css: string }[] = []
+
+  const comments: { value: string, endLine: number }[] = []
+  const nodes = findAll(
+    parse(input, {
+      positions: true,
+      // Comments are not part of the tree. We rely on the positions to infer the subset
+      onComment(value, loc) {
+        comments.push({ value: value.trim(), endLine: loc.end.line })
+      },
+    }),
+    node => node.type === 'Atrule' && node.name === 'font-face',
+  )
+
+  // If there are no comments, we don't associate subsets because we can't
+  if (comments.length === 0) {
+    return [{ subset: null, css: input }]
+  }
+
+  for (const node of nodes) {
+    const comment = comments.filter(comment => comment.endLine < node.loc!.start.line).at(-1)
+
+    data.push({ subset: comment?.value ?? null, css: generate(node) })
+  }
+
+  return data
 }
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { prepareWeights } from '../src/utils'
+import { prepareWeights, splitCssIntoSubsets } from '../src/utils'
 
 describe('utils', () => {
   describe('prepareWeights()', () => {
@@ -53,6 +53,65 @@ describe('utils', () => {
         { weight: '500', variable: false },
         { weight: '300', variable: false },
       ])
+    })
+  })
+
+  describe('splitCssIntoSubsets()', () => {
+    it('associates subsets and css correctly if there are comments', () => {
+      expect(
+        splitCssIntoSubsets(`
+/* vietnamese */
+@font-face {
+  font-family: 'A';
+}
+/* latin-ext */
+@font-face {
+  font-family: 'B';
+}
+@font-face {
+  font-family: 'Still B';
+}
+/* latin */
+@font-face {
+  font-family: 'C';
+}
+body {
+  --google-font-color-bungeetint:none;
+}
+@font-face {
+  font-family: 'Still C';
+}
+`),
+      ).toEqual([
+        { subset: 'vietnamese', css: '@font-face{font-family:"A"}' },
+        { subset: 'latin-ext', css: '@font-face{font-family:"B"}' },
+        { subset: 'latin-ext', css: '@font-face{font-family:"Still B"}' },
+        { subset: 'latin', css: '@font-face{font-family:"C"}' },
+        { subset: 'latin', css: '@font-face{font-family:"Still C"}' },
+      ])
+    })
+
+    it('it does not associate subsets if there are no comments', () => {
+      const input = `
+@font-face {
+  font-family: 'A';
+}
+@font-face {
+  font-family: 'B';
+}
+`
+
+      expect(splitCssIntoSubsets(input)).toEqual([{ subset: null, css: input }])
+    })
+
+    it('it does not associate subsets if comment is not in the right place', () => {
+      expect(splitCssIntoSubsets(`
+@font-face {
+  font-family: 'A';
+}
+/* latin */
+
+`)).toEqual([{ subset: null, css: '@font-face{font-family:"A"}' }])
     })
   })
 })
