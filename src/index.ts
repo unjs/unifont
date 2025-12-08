1d78704dca08ce6cc79272750a17258286fa8370
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-12-08 17:19:17 +0100

    Apply suggestion from @florian-lefebvre

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export type { GoogleFamilyOptions, GoogleOptions } from './providers/google'
 export type { GoogleiconsFamilyOptions, GoogleiconsOptions } from './providers/googleicons'
 export type { FontFaceData, FontFaceMeta, FontStyles, InitializedProvider, LocalFontSource, Provider, ProviderContext, ProviderDefinition, ProviderFactory, RemoteFontSource, ResolveFontOptions, ResolveFontResult } from './types'
 export type { Unifont, UnifontOptions } from './unifont'
+
 export { createUnifont, defaultResolveOptions } from './unifont'
 export { defineFontProvider } from './utils'
