38f6dd502761a58aed27c639ae844e778ca09965
Louis Shawn <louis.shawn@qq.com>
2026-01-23 15:22:42 +0800

    feat(providers): add npm provider

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * as providers from './providers'
 export type { AdobeProviderOptions } from './providers/adobe'
 export type { GoogleFamilyOptions, GoogleProviderOptions } from './providers/google'
 export type { GoogleiconsFamilyOptions, GoogleiconsProviderOptions } from './providers/googleicons'
+export type { NpmFamilyOptions, NpmProviderOptions } from './providers/npm'
 export type { FontFaceData, FontFaceMeta, FontStyles, InitializedProvider, LocalFontSource, Provider, ProviderContext, ProviderDefinition, ProviderFactory, RemoteFontSource, ResolveFontOptions, ResolveFontResult } from './types'
 export type { Unifont, UnifontOptions } from './unifont'
 
