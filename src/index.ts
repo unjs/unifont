8dee091c29edb8221b60b5564bf16e7dadf7c5eb
Daniel Roe <daniel@roe.dev>
2026-09-22 13:30:37 +0100

    fix!: export public types and drop deprecated ones

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
-import type { GoogleProviderOptions } from './providers/google'
-import type { GoogleiconsProviderOptions } from './providers/googleicons'
-
 export * as providers from './providers'
 export type { AdobeProviderOptions } from './providers/adobe'
 export type { GoogleFamilyOptions, GoogleProviderOptions } from './providers/google'
 export type { GoogleiconsFamilyOptions, GoogleiconsProviderOptions } from './providers/googleicons'
 export type { NpmFamilyOptions, NpmProviderOptions } from './providers/npm'
-export type { FontAxis, FontFaceData, FontFaceMeta, FontMetrics, FontProperties, FontStyles, InitializedProvider, LocalFontSource, Provider, ProviderContext, ProviderDefinition, ProviderFactory, ProviderResolveFontOptions, RemoteFontSource, ResolvedVariableAxis, ResolveFontOptions, ResolveFontResult, VariableAxis, VariableAxisValue } from './types'
+export type { FontAxis, FontFaceData, FontFaceMeta, FontFormat, FontMetrics, FontProperties, FontStyles, InitializedProvider, LocalFontSource, NormalizedVariableAxis, Provider, ProviderContext, ProviderDefinition, ProviderFactory, ProviderResolveFontOptions, RemoteFontSource, ResolvedVariableAxis, ResolveFontOptions, ResolveFontResult, VariableAxis, VariableAxisBound, VariableAxisValue } from './types'
 export type { Unifont, UnifontOptions } from './unifont'
 
 export { createUnifont, defaultResolveOptions } from './unifont'
 export { defineFontProvider } from './utils'
-
-/** @deprecated */
-export type GoogleOptions = GoogleProviderOptions
-/** @deprecated */
-export type GoogleiconsOptions = GoogleiconsProviderOptions
